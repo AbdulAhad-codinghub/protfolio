964eedf949e884be883040929b2b828d6216d6e3
B0AY-C0D3GR0UND <99433641+B0AY-C0D3GR0UND@users.noreply.github.com>
2022-06-01 20:51:47 +0500

    Done with the file and folder Structure

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -1,7 +1,13 @@
 import react from 'react';
-import  ReactDOM  from 'react';
-
+import  ReactDOM  from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App/>, document.getElementById('root')); 
+//older react syntax :
+// ReactDOM.render(<App/>, document.getElementById('root')); 
+
+//New version react thatswhy use this syntax
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App/>);
